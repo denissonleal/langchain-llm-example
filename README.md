d3909923b14b45f1c10851e349e650d02095dd47
Denisson Leal <denissonleal@gmail.com>
2024-08-17 16:21:11 +0100

    configure and install some basic requirements

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # langchain-llm-example
+
+## Run Project
+
+### Create Environment
+
+```bash
+cp .env.example .env
+```
+
+
+### Actvice Virtual Environment
+
+```bash
+source .venv/bin/activate
+```
---
+++ app/config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+os.getenv('OPENIA_API_KEY')
+
+config = {
+    'app_env': os.getenv('APP_ENV', default='prod'),
+    'llm_model': os.getenv('LLM_MODEL', default='gpt-3.5-turbo'),
+    'llm_api_key': os.getenv('LLM_API_KEY'),
+}
+
+if __name__ == '__main__':
+    print(config)
