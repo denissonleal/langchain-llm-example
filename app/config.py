6f7fe47cffb6a835159e053b3ba69f1b66e73d4f
Denisson Leal <denissonleal@gmail.com>
2024-08-30 19:21:19 +0100

    add variavel com o modelo do embeddings

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ config = {
     'app_env': os.getenv('APP_ENV', default='prod'),
     'llm_api_model': os.getenv('LLM_API_MODEL', default='gpt-3.5-turbo'),
     'llm_api_key': os.getenv('LLM_API_KEY'),
+    'embeddings_model': os.getenv('EMBEDDINGS_MODEL', default='gpt-3.5-turbo'),
 }
 
 if __name__ == '__main__':
